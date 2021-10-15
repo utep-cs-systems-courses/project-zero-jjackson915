990afda45f7b00de71b23ee0e1751361379db903
jjackson915 <jhjackson@miners.utep.edu>
2021-10-14 20:20:29 -0600

    finished arrow and fonts

--- draw_chars.c
+++ draw_chars.c
@@ -13,3 +13,15 @@ void print_char_11x16(char c)
     putchar('\n');
   }
 }
+void print_char_8x12(char c)
+{
+  c -= 0x20;
+  for (char col = 0; col < 8; col++) {
+    for (char row = 12; row > 0; row--) {
+      unsigned short rowBits = font_8x12[c][row];
+      unsigned short colMask = 1 << (7-col); /* mask to select bit associated with bit */
+      putchar( (rowBits & colMask) ? '*' : ' ');
+    }
+    putchar('\n');
+  }
+}
